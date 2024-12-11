fd825e6a0f9815df2aa3cdbd425c3997972a0739
Joeldo04 <joeladominguez04@gmail.com>
2024-12-11 11:50:45 -0500

    a

--- Ejemplos/src/paquete1/Ejemplo02.java
+++ Ejemplos/src/paquete1/Ejemplo02.java
@@ -16,7 +16,8 @@ public class Ejemplo02 {
      */
     public static void main(String[] args) {
         // Declarar y generar arreglos en Java
-
+        //imprime 0 por que la variable int no tienen un valor y es un arreglo de 
+        //tipo entero
         // arreglo de tipo entero
         int[] arreglo1 = new int[5];
         System.out.println(arreglo1[0]);
@@ -24,15 +25,15 @@ public class Ejemplo02 {
         System.out.println(arreglo1[2]);
         System.out.println(arreglo1[3]);
         System.out.println(arreglo1[4]);
-        
+        // imprime 0,0 por que es un dato de tipo decimal
         // arreglo de tipo double
         double[] arreglo2 = new double[5];
         System.out.println(arreglo2[0]);
-        
+        // imprime null por que es una variable de tipo cadena
         // arreglo de tipo String
         String[] arreglo4 = new String[5];
         System.out.println(arreglo4[0]);
-        
+        // imprime (f) por que es un tipo de dato Booleano V-F
         // arreglo de tipo boleano
         boolean[] arreglo5 = new boolean[5];
         System.out.println(arreglo5[0]);
